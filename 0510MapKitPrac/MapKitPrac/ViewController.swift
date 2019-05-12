b54e8d45cbf012102d6eafa64c038d87f25b5204
JeongAKo <kja2683@gmail.com>
2019-05-12 18:58:12 +0900

    Map Kit update

--- 0510MapKitPrac/MapKitPrac/ViewController.swift
+++ 0510MapKitPrac/MapKitPrac/ViewController.swift
@@ -6,87 +6,117 @@
 //  Copyright © 2019 고정아. All rights reserved.
 //
 
-import UIKit
+import CoreLocation
 import MapKit
+import UIKit
 
-final class Annotation: NSObject, MKAnnotation{
-    var title: String?
-    var coordinate: CLLocationCoordinate2D
+final class ViewController: UIViewController {
     
-    init(title: String, coordinate: CLLocationCoordinate2D) {
-        self.title = title
-        self.coordinate = coordinate
-    }
-}
-
-class ViewController: UIViewController {
+    let textFeild: UITextField = {
+        let textFeild = UITextField()
+        textFeild.font = UIFont.systemFont(ofSize: 25)
+        textFeild.backgroundColor = .white
+        textFeild.borderStyle = .roundedRect
+        return textFeild
+    }()
     
     let mapView = MKMapView()
+    
+    var placeNum = 0
+    
     private let locationManager = CLLocationManager()
+    
+    var latitude: CLLocationDegrees = 0.0
+    var longitude: CLLocationDegrees = 0.0
+    var nextDestinaton = CLLocationCoordinate2DMake(0.0, 0.0)
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(mapView)
-        autoLayout()
-        locationManager.delegate = self
-        mapView.showsUserLocation = true
-        mapView.mapType = .standard
-        checkAuthorizationStatus()
-    }
-    
-    func checkAuthorizationStatus() {
-        switch CLLocationManager.authorizationStatus() {
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .restricted, .denied :
-            break
-        case .authorizedWhenInUse:
-            fallthrough
-        case .authorizedAlways:
-            startUpdateLocation()
-            print("authorized")
-        @unknown default: break
-            
-        }
-    }
-    
-    func startUpdateLocation() {
-        let status = CLLocationManager.authorizationStatus()
-        guard status == .authorizedAlways || status == .authorizedWhenInUse, CLLocationManager.locationServicesEnabled()
-            else {return}
         
-        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager.distanceFilter = 10.0 //kCLDistanceFilterNone
-        locationManager.startUpdatingLocation()
+        view.addSubview(mapView)
+        mapView.addSubview(textFeild)
+        autoLayout()
+        textFeild.delegate = self
+        locationManager.delegate = self
+        //        mapView.showsUserLocation = true
+        mapView.mapType = .standard
+        
+    }
+    
+    func addAnnotation() {
+        
+    }
+    
+    func setRegion(coordinate: CLLocationCoordinate2D) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+ 
+    func moveToDestiantion(_ sender: Any) {
+        guard !mapView.annotations.isEmpty else {return}
+
+        let annotation = mapView.annotations[0]
+        setRegion(coordinate: annotation.coordinate)
         
     }
     
     func autoLayout() {
         
+        let guide = view.safeAreaLayoutGuide
         mapView.translatesAutoresizingMaskIntoConstraints = false
-        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        textFeild.translatesAutoresizingMaskIntoConstraints = false
+        textFeild.topAnchor.constraint(equalTo: guide.topAnchor, constant: 5).isActive = true
+        textFeild.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 5).isActive = true
+        textFeild.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -5).isActive = true
+        textFeild.heightAnchor.constraint(equalToConstant: 45).isActive = true
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        let text = textField.text
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString("\(text)", completionHandler: { (placeMark, error) in
+            guard let placeMark = placeMark?.first else {return}
+            
+            self.latitude = placeMark.location?.coordinate.latitude ?? 0.0
+            self.longitude =  placeMark.location?.coordinate.longitude ?? 0.0
+            self.mapView.addAnnotation(self.nextDestinaton as! MKAnnotation)
+            
+//            print("\(placeMark.location?.coordinate.latitude), \(placeMark.location?.coordinate.longitude)")
+        })
+        
+        return true
+    }
+}
+
+
 extension ViewController: CLLocationManagerDelegate {
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currernt = locations.last!
         
-        let current = locations.last!
-        
-        if (abs(current.timestamp.timeIntervalSinceNow) < 10 ){
-            let coordinate = current.coordinate
-            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        if (abs(currernt.timestamp.timeIntervalSinceNow) < 10 ){
+            let coordinate = currernt.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.0001, longitudeDelta: 0.0001)
             let region = MKCoordinateRegion(center: coordinate, span: span)
             mapView.setRegion(region, animated: true)
-            addAnnotation(location: current)
+//            addAnnotation(location: currernt)
         }
     }
     
-    func addAnnotation(location: CLLocation) {
-        let annotation = Annotation(title: "My Location", coordinate: location.coordinate)
-        mapView.addAnnotation(annotation)
-    }
+//    func addAnnotation(location: CLLocation) {
+//        let annotation = Annotation(title: "My Location", coordinate: location.coordinate)
+//        mapView.addAnnotation(annotation)
+//    }
 }
