f3877648819b2ec22b3b3747435e20db38eab486
JeongAKo <kja2683@gmail.com>
2019-05-14 22:15:28 +0900

    0510 MapKitPrac 완성

--- 0510MapKitPrac/MapKitPrac/ViewController.swift
+++ 0510MapKitPrac/MapKitPrac/ViewController.swift
@@ -6,135 +6,87 @@
 //  Copyright © 2019 고정아. All rights reserved.
 //
 
-
-import MapKit
 import UIKit
+import MapKit
 
-final class ViewController: UIViewController {
+final class Annotation: NSObject, MKAnnotation{
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
     
-    let textFeild: UITextField = {
-        let textFeild = UITextField()
-        textFeild.font = UIFont.systemFont(ofSize: 25)
-        textFeild.backgroundColor = .white
-        textFeild.borderStyle = .roundedRect
-        return textFeild
-    }()
+    init(title: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.coordinate = coordinate
+    }
+}
 
+class ViewController: UIViewController {
+    
     let mapView = MKMapView()
-    let temp = CLLocationCoordinate2D()
-    var placeCount = 0
+    private let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.addSubview(mapView)
-        mapView.addSubview(textFeild)
         autoLayout()
-        textFeild.delegate = self
+        locationManager.delegate = self
+        mapView.showsUserLocation = true
         mapView.mapType = .standard
+        checkAuthorizationStatus()
     }
     
-    func addAnnotation(_ stringAdress:String, _ geoAdress:CLPlacemark) {
-        // MapView delegate
-        mapView.delegate = self
-        
-        // 장소 count 세기
-        placeCount += 1
-        
-        // 위도, 경도 값 저장
-        let latitude = geoAdress.location!.coordinate.latitude
-        let longitude = geoAdress.location!.coordinate.longitude
-
-         // addAnnotaition의 위치 정보 입력
-        let nextDestination = MKPointAnnotation()
-        nextDestination.title = "\(placeCount)번째 행선지"
-        nextDestination.subtitle = stringAdress
-        nextDestination.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
-        mapView.addAnnotation(nextDestination)
-        
-        // span, region 설정
-        let span = MKCoordinateSpan.init(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        let region = MKCoordinateRegion.init(center: geoAdress.location!.coordinate, span: span)
-        mapView.setRegion(region, animated: true)
-        
-        if placeCount == 1 {
+    func checkAuthorizationStatus() {
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied :
+            break
+        case .authorizedWhenInUse:
+            fallthrough
+        case .authorizedAlways:
+            startUpdateLocation()
+            print("authorized")
+        @unknown default: break
             
-        } else {
-            addLine(temp, geoAdress.location!.coordinate)
         }
-        
-        addRectangle(geoAdress.location!.coordinate)
     }
     
-    func addLine(_ firstsender: CLLocationCoordinate2D, _ secondSender: CLLocationCoordinate2D) {
+    func startUpdateLocation() {
+        let status = CLLocationManager.authorizationStatus()
+        guard status == .authorizedAlways || status == .authorizedWhenInUse, CLLocationManager.locationServicesEnabled()
+            else {return}
         
-        let point1 = firstsender
-        let point2 = secondSender
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.distanceFilter = 10.0 //kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
         
-        let points: [CLLocationCoordinate2D] = [point1, point2]
-        let polyline = MKPolyline(coordinates: points, count: points.count)
-        mapView.addOverlay(polyline)
-    }
-    
-    func addRectangle(_ sender: CLLocationCoordinate2D) {
-        let center = sender
-        
-        var point1 = center; point1.longitude += 0.01;  point1.latitude -= 0.01;
-        var point2 = center; point2.longitude += 0.01;  point2.latitude -= 0.01;
-        var point3 = center; point3.longitude += 0.01;  point3.latitude -= 0.01;
-        var point4 = center; point4.longitude += 0.01;  point4.latitude -= 0.01;
-    
-    
-    }
-    
-    
-    // 맵 검색 ( geocoder를 이용하여 주소를 좌표로 변환 )
-    func searchMap(_ adrr: String) {
-        
-        let geocoder = CLGeocoder()
-        
-        geocoder.geocodeAddressString(adrr) { (placeMark, error) in
-            if error != nil {
-                return print(error?.localizedDescription)
-            }
-            guard let adress = placeMark?.first else {return}
-            self.addAnnotation(adrr, adress)
-        }
     }
     
     func autoLayout() {
         
-        let guide = view.safeAreaLayoutGuide
         mapView.translatesAutoresizingMaskIntoConstraints = false
-        mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        
-        textFeild.translatesAutoresizingMaskIntoConstraints = false
-        textFeild.topAnchor.constraint(equalTo: guide.topAnchor, constant: 5).isActive = true
-        textFeild.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 5).isActive = true
-        textFeild.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -5).isActive = true
-        textFeild.heightAnchor.constraint(equalToConstant: 45).isActive = true
     }
 }
 
-
-// textField 에서 값 가져오기
-extension ViewController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        guard let text = textField.text else { return false }
-        // textField의 값이 있을때, searchMap함수 호출
-        searchMap(text)
-        // 키보드 내리기
-        textField.resignFirstResponder()
-
-        return true
+        let current = locations.last!
+        
+        if (abs(current.timestamp.timeIntervalSinceNow) < 10 ){
+            let coordinate = current.coordinate
+            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+            addAnnotation(location: current)
+        }
     }
-}
-
-
-extension ViewController: MKMapViewDelegate {
     
+    func addAnnotation(location: CLLocation) {
+        let annotation = Annotation(title: "My Location", coordinate: location.coordinate)
+        mapView.addAnnotation(annotation)
+    }
 }
