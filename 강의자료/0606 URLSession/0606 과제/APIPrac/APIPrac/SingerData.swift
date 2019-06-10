95cf670ef0d333fe0f7422f4951182bee3a0996c
JeongAKo <kja2683@gmail.com>
2019-06-10 19:31:47 +0900

    searchBar 활용 API사용하기

---
+++ 강의자료/0606 URLSession/0606 과제/APIPrac/APIPrac/SingerData.swift
@@ -0,0 +1,16 @@
+//
+//  SingerData.swift
+//  APIPrac
+//
+//  Created by Daisy on 09/06/2019.
+//  Copyright © 2019 고정아. All rights reserved.
+//
+
+import UIKit
+
+struct Singer {
+    let artistName: String
+    let trackName: String
+    let artworkUrl100: UIImage
+}
+
